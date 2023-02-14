45b2a38eb6b022deef235e66cf43ecc94576940b
Peter Somerville <peter@geckoengage.com>
2023-02-14 22:29:58 +0000

    cors allowed_origins

--- config/app.php
+++ config/app.php
@@ -58,6 +58,8 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
+    'web_url' => env('WEB_URL', 'http://localhost:3000'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
