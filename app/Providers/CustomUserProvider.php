43a684c3b37547dce40848d34a028e8e4e26ae15
petersgecko <petersgecko@users.noreply.github.com>
2023-09-05 21:43:28 +0000

    Fix styling

--- app/Providers/CustomUserProvider.php
+++ app/Providers/CustomUserProvider.php
@@ -22,7 +22,7 @@ final class CustomUserProvider implements UserProvider
 
     public function retrieveByToken($identifier, $token)
     {
-        return null;
+        return;
     }
 
     public function updateRememberToken(Authenticatable $user, $token)
