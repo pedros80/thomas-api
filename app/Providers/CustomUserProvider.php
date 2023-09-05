139b453f86b124eb3af8f91e75e7e910aa3faa04
Peter Somerville <peter@geckoengage.com>
2023-09-05 22:45:54 +0100

    stan fixes

--- app/Providers/CustomUserProvider.php
+++ app/Providers/CustomUserProvider.php
@@ -22,7 +22,7 @@ final class CustomUserProvider implements UserProvider
 
     public function retrieveByToken($identifier, $token)
     {
-        return;
+        //
     }
 
     public function updateRememberToken(Authenticatable $user, $token)
